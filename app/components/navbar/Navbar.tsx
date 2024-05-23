bcbb0c04bc5b2cd64dba369af142ebb1b4d40cec
samlee088 <108655429+samlee088@users.noreply.github.com>
2024-05-22 21:06:46 -0700

    initial set up for nav bar

---
+++ app/components/navbar/Navbar.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <nav className="w-full fixed top-0 left-0 py-6 border-b bg-white z-10">
+      hello nav bar
+    </nav>
+  );
+};
+
+export default Navbar;
