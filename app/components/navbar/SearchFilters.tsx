948aacdc3f3dc86efceec792c4ddf3287bc5e5ef
samlee088 <108655429+samlee088@users.noreply.github.com>
2024-05-24 08:10:44 -0700

    search filters search box css and intial set up

--- app/components/navbar/SearchFilters.tsx
+++ app/components/navbar/SearchFilters.tsx
@@ -1,7 +1,17 @@
 import React from "react";
 
 const SearchFilters = () => {
-  return <div>Search filters</div>;
+  return (
+    <div className="h-[64px] flex flex-row items-center justify-between border rounded-full">
+      <div>
+        <div className="flex flex-row items-center justify-between">
+          <div className="w-[250px] h-[64px] px-8 flex flex-columns rounded-full hover:bg-gray-100">
+            Where
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default SearchFilters;
