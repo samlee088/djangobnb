d97e8f3ecd80acf4d563f0f9611f6e655ea03950
samlee088 <108655429+samlee088@users.noreply.github.com>
2024-05-25 15:57:47 -0700

    property list image

--- app/components/properties/PropertyListItem.tsx
+++ app/components/properties/PropertyListItem.tsx
@@ -1,7 +1,20 @@
+import Image from "next/image";
 import React from "react";
 
 const PropertyListItem = () => {
-  return <div>hello</div>;
+  return (
+    <div className="cursor-pointer">
+      <div className="relative overflow-hidden aspect-square rounded-xl">
+        <Image
+          fill
+          src="/beach_1.jpg"
+          sizes="(max-width: 768px): 768px, (max-width: 1200px): 768px, 768px"
+          className="hover:scale-110 object-cover transition h-full w-full"
+          alt="beach house"
+        />
+      </div>
+    </div>
+  );
 };
 
 export default PropertyListItem;
