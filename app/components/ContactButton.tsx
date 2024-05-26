9109db0f9635b85c08e5515c54708bf517c2b535
samlee088 <108655429+samlee088@users.noreply.github.com>
2024-05-25 18:25:13 -0700

    contact button created

---
+++ app/components/ContactButton.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const ContactButton = () => {
+  return (
+    <div className="mt-6 py-6 px-6 cursor-pointer hover:bg-airbnbDark bg-airbnb transition text-white rounded-xl">
+      {" "}
+      Contact
+    </div>
+  );
+};
+
+export default ContactButton;
--- app/components/landlords/[id]/page.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const LandlordDetailPage = () => {
-  return <main className="max-w-[1500px] mx-auto px-6 pb-6"></main>;
-};
-
-export default LandlordDetailPage;
