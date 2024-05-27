c1908ea28d014ed7f02735c123d244463dbf7d1b
samlee088 <108655429+samlee088@users.noreply.github.com>
2024-05-27 16:16:06 -0700

    conversations list page set up

---
+++ app/components/inbox/Conversation.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Conversation = () => {
+  return (
+    <div className="px-6 py-4 cursor-pointer border border-gray-300 rounded-xl">
+      <p className="mb-6 text-xl">John Doe</p>
+
+      <p className="text-airbnbDark">Go to conversation</p>
+    </div>
+  );
+};
+
+export default Conversation;
--- app/inbox/page.tsx
+++ app/inbox/page.tsx
@@ -1,10 +1,15 @@
 import React from "react";
 import PropertyList from "../components/properties/PropertyList";
+import Conversation from "../components/inbox/Conversation";
 
 const InboxPage = () => {
   return (
-    <main className="max-w-[1500px] mx-auto px-6 pb-6">
+    <main className="max-w-[1500px] mx-auto px-6 pb-6 space-y-4">
       <h1 className="my-6 text-2xl">Inbox</h1>
+
+      <Conversation />
+      <Conversation />
+      <Conversation />
     </main>
   );
 };
