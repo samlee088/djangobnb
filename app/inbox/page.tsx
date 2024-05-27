ad88cd0dc90d52743be830b359397f1cc82d98d9
samlee088 <108655429+samlee088@users.noreply.github.com>
2024-05-27 16:12:43 -0700

    intial inbox page created

---
+++ app/inbox/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import PropertyList from "../components/properties/PropertyList";
+
+const InboxPage = () => {
+  return (
+    <main className="max-w-[1500px] mx-auto px-6 pb-6">
+      <h1 className="my-6 text-2xl">Inbox</h1>
+    </main>
+  );
+};
+
+export default InboxPage;
