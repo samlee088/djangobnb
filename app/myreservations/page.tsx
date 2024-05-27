ffa119e32047f233187233e78e9a90ac8416b2ad
samlee088 <108655429+samlee088@users.noreply.github.com>
2024-05-27 15:53:49 -0700

    set up for reservations list

--- app/myreservations/page.tsx
+++ app/myreservations/page.tsx
@@ -18,20 +18,24 @@ const MyReservationsPage = () => {
             </div>
           </div>
 
-          <div className="col-span-3 space-y-2">
+          <div className="col-span-3">
             <h2 className="mb-4 text-xl">Property name</h2>
-            <p>
+            <p className="mb-2">
               <strong>Check in date:</strong> 14/2/2024
             </p>
-            <p>
+            <p className="mb-2">
               <strong>Check out date:</strong> 16/2/2024
             </p>
-            <p>
+            <p className="mb-2">
               <strong>Number of nights:</strong> 2
             </p>
-            <p>
+            <p className="mb-2">
               <strong>Total Price:</strong> $200
             </p>
+
+            <div className="mt-6 inline-block cursor-pointer py-4 px-6 bg-airbnb text-white rounded-xl">
+              Go to property
+            </div>
           </div>
         </div>
         <div className="p-5 grid grid-cols-1 md:grid-cols-4 gap-4 shadow-md border border-gray-300">
@@ -46,20 +50,24 @@ const MyReservationsPage = () => {
             </div>
           </div>
 
-          <div className="col-span-3 space-y-2">
+          <div className="col-span-3">
             <h2 className="mb-4 text-xl">Property name</h2>
-            <p>
+            <p className="mb-2">
               <strong>Check in date:</strong> 14/2/2024
             </p>
-            <p>
+            <p className="mb-2">
               <strong>Check out date:</strong> 16/2/2024
             </p>
-            <p>
+            <p className="mb-2">
               <strong>Number of nights:</strong> 2
             </p>
-            <p>
+            <p className="mb-2">
               <strong>Total Price:</strong> $200
             </p>
+
+            <div className="mt-6 inline-block cursor-pointer py-4 px-6 bg-airbnb text-white rounded-xl">
+              Go to property
+            </div>
           </div>
         </div>
       </div>
